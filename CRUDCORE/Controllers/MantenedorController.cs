29b9fd4f58b6d225696f82c52dce75986230f189
BryanJaraGit <brjara77@gmail.com>
2022-08-25 21:41:10 -0500

    Agregar archivos de proyecto.

---
+++ CRUDCORE/Controllers/MantenedorController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using CRUDCORE.Datos;
+using CRUDCORE.Models;
+
+namespace CRUDCORE.Controllers
+{
+    public class MantenedorController : Controller
+    {
+        ContactoDatos _ContactoDatos = new ContactoDatos();
+
+        //LA VISTA MOSTRARA UNA LISTA DE CONTACTOS
+        public IActionResult Listar()
+        {
+            var oLista = _ContactoDatos.Listar();
+
+            return View(oLista);
+        }
+
+        //METODO SOLO DEVUELVE LA VISTA
+        public IActionResult Guardar()
+        {
+            return View();
+        }
+
+
+
+        //METODO RECIBE EL OBJETO PARA GUARDARLO EN BD
+        [HttpPost]
+        public IActionResult Guardar(ContactoModel oContacto)
+        {
+
+            if (!ModelState.IsValid)
+                return View();
+
+            var respuesta = _ContactoDatos.Guardar(oContacto);
+
+            if(respuesta)
+                return RedirectToAction("Listar");
+            else
+                return View();
+        }
+
+        public IActionResult Editar(int IdContacto)
+        {
+            var ocontacto = _ContactoDatos.Obtener(IdContacto);
+            return View(ocontacto);
+        }
+
+        [HttpPost]
+        public IActionResult Editar(ContactoModel oContacto)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            var respuesta = _ContactoDatos.Editar(oContacto);
+
+            if (respuesta)
+                return RedirectToAction("Listar");
+            else
+                return View();
+        }
+
+        public IActionResult Eliminar(int IdContacto)
+        {
+            var ocontacto = _ContactoDatos.Obtener(IdContacto);
+            return View(ocontacto);
+        }
+
+        [HttpPost]
+        public IActionResult Eliminar(ContactoModel oContacto)
+        {
+            
+            var respuesta = _ContactoDatos.Eliminar(oContacto.IdContacto);
+
+            if (respuesta)
+                return RedirectToAction("Listar");
+            else
+                return View();
+        }
+    }
+}
